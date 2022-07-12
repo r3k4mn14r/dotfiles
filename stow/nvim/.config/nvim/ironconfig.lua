f226dcaf9f2bebe569622086691240dccb37a5d6
r3k4mn14r <8102483+r3k4mn14r@users.noreply.github.com>
2022-07-12 13:50:09 -0500

    updated iron config

--- stow/nvim/.config/nvim/ironconfig.lua
+++ stow/nvim/.config/nvim/ironconfig.lua
@@ -1,9 +1,16 @@
-local iron = require('iron')
+local iron = require('iron.core')
 
-iron.core.set_config {
-  preferred = {
-    python = "ipython",
+iron.setup {
+  config = {
+    close_window_on_exit = true,
+    python = require("iron.fts.python").ipython,
+    -- should_map_plug = true,
+    repl_open_cmd = "botright vertical split",
   },
-  repl_open_cmd = "botright vertical split"
 
+  keymaps = {
+    send_motion = "<space>sc",
+    visual_send = "<space>sc",
+  }
 }
+
