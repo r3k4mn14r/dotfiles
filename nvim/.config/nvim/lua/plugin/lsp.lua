9b9e5d3d24380ec4800b84a64c8a67c617497c1b
r3k4mn14r <8102483+r3k4mn14r@users.noreply.github.com>
2022-07-27 18:46:49 -0500

    added back some configs

--- nvim/.config/nvim/lua/plugin/lsp.lua
+++ nvim/.config/nvim/lua/plugin/lsp.lua
@@ -1,15 +1,3 @@
--- utility functions
-
-local function CreateNoremap(type, opts)
-	return function(lhs, rhs, bufnr)
-        bufnr = bufnr or 0
-		vim.api.nvim_buf_set_keymap(bufnr, type, lhs, rhs, opts)
-	end
-end
-
-Nnoremap = CreateNoremap("n", { noremap = true })
-Inoremap = CreateNoremap("i", { noremap = true })
-
 -- lsp configurations
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -33,9 +21,9 @@ cmp.setup({
 		end,
 	},
 	mapping = cmp.mapping.preset.insert({
-    ['<Tab>'] = cmp.mapping.select_next_item(),
-    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ['<Tab>'] = cmp.mapping.select_next_item(),
+        ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
 		["<C-u>"] = cmp.mapping.scroll_docs(-4),
 		["<C-d>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
@@ -61,18 +49,18 @@ local function config(_config)
 	return vim.tbl_deep_extend("force", {
 		capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
 		on_attach = function()
-      -- See `:help vim.lsp.*` for documentation on any of the below functions
-			Nnoremap("gd", ":lua vim.lsp.buf.definition()<CR>")
-			Nnoremap("K", ":lua vim.lsp.buf.hover()<CR>")
-			Nnoremap("gi", ":lua vim.lsp.buf.implementation()<CR>")
-			Nnoremap("[d", ":lua vim.lsp.diagnostic.goto_next()<CR>")
-			Nnoremap("]d", ":lua vim.lsp.diagnostic.goto_prev()<CR>")
-			Nnoremap("<leader>vd", ":lua vim.diagnostic.open_float()<CR>")
-			Nnoremap("<leader>vws", ":lua vim.lsp.buf.workspace_symbol()<CR>")
-			Nnoremap("<leader>vca", ":lua vim.lsp.buf.code_action()<CR>")
-			Nnoremap("<leader>vrr", ":lua vim.lsp.buf.references()<CR>")
-			Nnoremap("<leader>vrn", ":lua vim.lsp.buf.rename()<CR>")
-			Inoremap("<C-h>", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
+            -- See `:help vim.lsp.*` for documentation on any of the below functions
+			nmap("gd", ":lua vim.lsp.buf.definition()<CR>")
+			nmap("K", ":lua vim.lsp.buf.hover()<CR>")
+			nmap("gi", ":lua vim.lsp.buf.implementation()<CR>")
+			nmap("[d", ":lua vim.lsp.diagnostic.goto_next()<CR>")
+			nmap("]d", ":lua vim.lsp.diagnostic.goto_prev()<CR>")
+			nmap("<leader>vd", ":lua vim.diagnostic.open_float()<CR>")
+			nmap("<leader>vws", ":lua vim.lsp.buf.workspace_symbol()<CR>")
+			nmap("<leader>vca", ":lua vim.lsp.buf.code_action()<CR>")
+			nmap("<leader>vrr", ":lua vim.lsp.buf.references()<CR>")
+			nmap("<leader>vrn", ":lua vim.lsp.buf.rename()<CR>")
+			imap("<C-h>", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
 		end,
 	}, _config or {})
 end
---
+++ nvim/.config/nvim/lua/plugin/telescope.lua
@@ -0,0 +1,4 @@
+nmap("<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<cr>")
+nmap("<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<cr>")
+nmap("<leader>fb", "<cmd>lua require('telescope.builtin').buffers()<cr>")
+nmap("<leader>fh", "<cmd>lua require('telescope.builtin').help_tags()<cr>")
