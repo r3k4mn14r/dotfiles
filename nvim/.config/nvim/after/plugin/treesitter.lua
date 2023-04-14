85c9739005b30b8e0ddc14399f4d7aa1f7a44f87
r3k4mn14r <8102483+r3k4mn14r@users.noreply.github.com>
2023-04-14 08:23:38 -0400

    removed mandatory typescript LSP server install

--- nvim/.config/nvim/after/plugin/treesitter.lua
+++ nvim/.config/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require('nvim-treesitter.configs').setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "python", "rust", "lua", "typescript", "help" },
+  ensure_installed = { "python", "rust", "lua" },
   -- Automatically install missing parsers when entering buffer
   auto_install = true,
   highlight = { enable = true },
