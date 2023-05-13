cc9bd49bfb8c83a48f18e21caf1860cb38699d15
r3k4mn14r <8102483+r3k4mn14r@users.noreply.github.com>
2023-05-13 13:36:58 -0400

    fixed neovim colorscheme

--- nvim/.config/nvim/after/plugin/colors.lua
+++ nvim/.config/nvim/after/plugin/colors.lua
@@ -1,6 +1,7 @@
 vim.opt.termguicolors = true
 rawset(require("colorbuddy").styles, "italic", require("colorbuddy").styles.none)
-require('colorbuddy').colorscheme('gruvbuddy')
+
+require("colorbuddy").colorscheme "gruvbuddy"
 require("colorizer").setup()
 
 local c = require("colorbuddy.color").colors
@@ -8,10 +9,7 @@ local Group = require("colorbuddy.group").Group
 local g = require("colorbuddy.group").groups
 local s = require("colorbuddy.style").styles
 
-Group.new("GoTestSuccess", c.green, nil, s.bold)
-Group.new("GoTestFail", c.red, nil, s.bold)
-
--- Group.new('Keyword', c.purple, nil, nil)
+Group.new("@variable", c.superwhite, nil)
 
 Group.new("TSPunctBracket", c.orange:light():light())
 
@@ -22,12 +20,10 @@ Group.new("StatuslineError3", c.red:dark(), g.Statusline)
 Group.new("StatuslineError3", c.red:dark():dark(), g.Statusline)
 
 Group.new("pythonTSType", c.red)
-Group.new("goTSType", g.Type.fg:dark(), nil, g.Type)
 
 Group.new("typescriptTSConstructor", g.pythonTSType)
 Group.new("typescriptTSProperty", c.blue)
 
--- vim.cmd [[highlight WinSeparator guifg=#4e545c guibg=None]]
 Group.new("WinSeparator", nil, nil)
 
 Group.new("TSTitle", c.blue)
@@ -36,3 +32,15 @@ Group.new("InjectedLanguage", nil, g.Normal.bg:dark())
 
 Group.new("LspParameter", nil, nil, s.italic)
 Group.new("LspDeprecated", nil, nil, s.strikethrough)
+Group.new("@function.bracket", g.Normal, g.Normal)
+Group.new("@variable.builtin", c.purple:light():light(), g.Normal)
+
+Group.new("VirtNonText", c.gray3:dark(), nil, s.italic)
+
+Group.new("TreesitterContext", nil, g.Normal.bg:light())
+Group.new("TreesitterContextLineNumber", c.blue)
+
+Group.new("@property", c.blue)
+Group.new("@punctuation.bracket.rapper", c.gray3, nil, s.none)
+Group.new("@rapper_argument", c.red, nil, s.italic)
+Group.new("@rapper_return", c.orange:light(), nil, s.italic)
